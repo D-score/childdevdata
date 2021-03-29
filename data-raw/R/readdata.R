273b35d05bff452fb9502642a261570884f8e78d
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-03-29 15:49:47 +0200

    Move to source data to data-raw

---
+++ data-raw/R/readdata.R
@@ -0,0 +1,19 @@
+library(readr)
+
+studies <- c("gcdg_col_lt42m", "gcdg_col_lt45m", "gcdg_ecu", "gcdg_jam_lbw",
+             "gcdg_jam_stunted", "gcdg_mdg", "gcdg_nld_smocc", "gcdg_zaf")
+paths <- file.path("data-raw", "data", paste(studies, "txt", sep = "."))
+
+data <- vector("list", length(studies))
+names(data) <- studies
+for (k in 1:length(studies)) {
+  varnames <- colnames(read_tsv(paths[k], col_types = cols(), n_max = 0))
+  types <- paste0(c("cciiici",
+                    paste0(rep("i", length(varnames) - 7), collapse = "")), collapse = "")
+  data[[studies[k]]] <- read_tsv(paths[k], col_types = types)
+  assign(studies[k], data[[studies[k]]])
+}
+
+usethis::use_data(gcdg_col_lt42m, gcdg_col_lt45m, gcdg_ecu, gcdg_jam_lbw,
+                  gcdg_jam_stunted, gcdg_mdg, gcdg_nld_smocc, gcdg_zaf,
+                  overwrite = TRUE)
